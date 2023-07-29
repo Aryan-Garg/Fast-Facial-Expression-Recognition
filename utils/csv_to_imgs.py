c2d370044b75cd05f4c16ea5b22fd7a826d74b4e
Aryan-Garg <aryangarg019@gmail.com>
2023-07-29 16:24:50 +0530

    Save data with class dirs (csv_to_imgs.py) | Finish EDA with cleaner dataset | Add baselines NB with models to test

--- utils/csv_to_imgs.py
+++ utils/csv_to_imgs.py
@@ -3,12 +3,16 @@
 import os
 import sys
 sys.path.append('../')
+import shutil
 
 import numpy as np 
 import pandas as pd
 from tqdm import tqdm
 import glob
 import cv2
+
+class2emotion = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
+emotion2class = dict((v,k) for k,v in class2emotion.items())
 
 
 def create_image_from_rmo_list(pixel_lst):
@@ -24,18 +28,19 @@ def make_if_not_dir(dir_path):
         os.makedirs(dir_path)
     else:
         print('Directory already exists.')
-        # Clean based on user input
-        while True:
-            user_input = input('Clean directory? [y/n]: ')
-            if user_input == 'y':
-                files = glob.glob(dir_path + '*')
-                for f in files:
-                    os.remove(f)
-                break
-            elif user_input == 'n':
-                break
-            else:
-                print('Invalid input. Try again.')
+        # take user input to remove for existing directory
+        ans = input("Do you want to remove the existing directory (y/n):")
+        if ans == 'y':
+            # remove existing directory
+            shutil.rmtree(dir_path, ignore_errors=True)
+            print('Directory removed.')
+            # create new directory
+            os.makedirs(dir_path)
+        else:
+            # exit the script
+            print('Existing directory was not removed. Exiting script.')
+            sys.exit()
+
 
 
 if __name__ == '__main__':
@@ -49,10 +54,16 @@ if __name__ == '__main__':
     make_if_not_dir(SAVE_TEST_DIR)
 
     print('Saving train images...')
+    cnt_classes = {0: 0, 1: 0, 2: 0, 3: 0, 4:0, 5:0, 6:0}
     for i in tqdm(range(len(train_csv))):
         img = create_image_from_rmo_list(train_csv['pixels'][i].split(' '))
         annotation = train_csv['emotion'][i]
-        cv2.imwrite(SAVE_TRAIN_DIR + str(i) + '.png', img) # png to not degrade quality as in jpg!!
+        emotion = class2emotion[annotation]
+        if not os.path.exists(SAVE_TRAIN_DIR + f"{emotion}"):
+            os.makedirs(SAVE_TRAIN_DIR + f"{emotion}")
+        cv2.imwrite(SAVE_TRAIN_DIR + f"{emotion}/{emotion}_" + str(cnt_classes[annotation]) + '.png', img)
+        cnt_classes[train_csv['emotion'][i]] += 1
+
         with open(DATA_PATH + 'train_annotations.txt', 'a') as f:
             f.write(str(annotation))
 
