652189a3df651b2688309bfaa50a65816e02df27
Aryan-Garg <aryangarg019@gmail.com>
2023-07-29 11:38:36 +0530

    Add reading, length statistics, processing and conversion to images with annotation file creation (++fastai batch grid visualization)

--- utils/csv_to_imgs.py
+++ utils/csv_to_imgs.py
@@ -2,28 +2,61 @@
 
 import os
 import sys
-sys.append('../')
+sys.path.append('../')
 
 import numpy as np 
-import cv2
 import pandas as pd
 from tqdm import tqdm
 import glob
+import cv2
 
 
-def csv_to_imgs(csv_path, img_path, img_size, img_type):
-    """
-    csv_path: path to the csv file
-    img_path: path to the folder where images are stored
-    img_size: size of the image
-    img_type: type of the image
-    """
-    df = pd.read_csv(csv_path)
-    for i in tqdm(range(df.shape[0])):
-        img_name = df.iloc[i, 0]
-        img = cv2.imread(os.path.join(img_path, img_name), 0)
-        # img = cv2.resize(img, (img_size, img_size))
-        # cv2.imwrite(os.path.join(img_path, img_name.split('.')[0] + img_type), img)
+def create_image_from_rmo_list(pixel_lst):
+    img = np.zeros((48, 48), dtype=np.uint8)
+    for i in range(48):
+        for j in range(48):
+            img[i][j] = int(pixel_lst[i * 48 + j])
+    return img
+
+
+def make_if_not_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    else:
+        print('Directory already exists.')
+        # Clean based on user input
+        while True:
+            user_input = input('Clean directory? [y/n]: ')
+            if user_input == 'y':
+                files = glob.glob(dir_path + '*')
+                for f in files:
+                    os.remove(f)
+                break
+            elif user_input == 'n':
+                break
+            else:
+                print('Invalid input. Try again.')
+
 
 if __name__ == '__main__':
-    csv_to_imgs('../dataset_FER/train.csv', '../data/train', 48, '.jpg')
+    DATA_PATH = 'dataset_FER/'
+    train_csv = pd.read_csv(DATA_PATH + 'renamed_train.csv')
+    test_csv = pd.read_csv(DATA_PATH + 'test.csv')
+
+    SAVE_TRAIN_DIR = DATA_PATH + 'train/'
+    SAVE_TEST_DIR = DATA_PATH + 'test/'
+    make_if_not_dir(SAVE_TRAIN_DIR)
+    make_if_not_dir(SAVE_TEST_DIR)
+
+    print('Saving train images...')
+    for i in tqdm(range(len(train_csv))):
+        img = create_image_from_rmo_list(train_csv['pixels'][i].split(' '))
+        annotation = train_csv['emotion'][i]
+        cv2.imwrite(SAVE_TRAIN_DIR + str(i) + '.png', img) # png to not degrade quality as in jpg!!
+        with open(DATA_PATH + 'train_annotations.txt', 'a') as f:
+            f.write(str(annotation))
+
+    print('Saving test images...')
+    for i in tqdm(range(len(test_csv))):
+        img = create_image_from_rmo_list(test_csv['pixels'][i].split(' '))
+        cv2.imwrite(SAVE_TEST_DIR + str(i) + '.png', img)
