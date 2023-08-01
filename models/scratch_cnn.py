deb0bbfaa2e44e4a0aa644a2224bb5d0b2e750f3
Aryan-Garg <aryangarg019@gmail.com>
2023-08-01 13:03:18 +0530

    Train Custom CNN

--- models/scratch_cnn.py
+++ models/scratch_cnn.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from torchsummary import summary
+
 
 class ScratchCNN(nn.Module):
     def __init__(self):
@@ -11,21 +13,34 @@ class ScratchCNN(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.relu3 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(in_features=64*6*6, out_features=1024)
+        self.fc1 = nn.Linear(in_features=64*28*28, out_features=1024)
         self.relu4 = nn.ReLU()
         self.fc2 = nn.Linear(in_features=1024, out_features=512)
         self.relu5 = nn.ReLU()
         self.fc3 = nn.Linear(in_features=512, out_features=7)
         self.softmax = nn.Softmax(dim=1)
 
-        self.model = nn.Sequential(
-            self.conv1, self.relu1, self.pool,
-            self.conv2, self.relu2, self.pool,
-            self.conv3, self.relu3, self.pool,
-            self.fc1, self.relu4,
-            self.fc2, self.relu5,
-            self.fc3, self.softmax
-        )
 
     def forward(self, x):
-        return self.model(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool(x)
+        # print(x.shape)
+        x = x.view(x.size(0), -1)
+        # print(x.shape)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.fc2(x)
+        x = self.relu5(x)
+        x = self.fc3(x)
+        x = self.softmax(x)
+        return x
+
+# cnn = ScratchCNN().to('cuda')
+# summary(cnn, (3, 224, 224))
